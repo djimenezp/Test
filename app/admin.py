a5ffe7d3ca98c3fd3e623c4b86d5fbcfc3569a83
David <d900929.jp@gmail.com>
2023-01-20 00:38:52 +0100

    Register models in admin site

--- app/admin.py
+++ app/admin.py
@@ -1,1 +1,27 @@
 # Register your models here.
+from django.contrib import admin
+
+from app import models
+
+
+class Default(admin.ModelAdmin):
+    """
+    Default model admin class
+    """
+    pass
+
+
+@admin.register(models.Item)
+class ItemAdmin(Default):
+    list_display = ['__str__', 'color', 'status']
+    list_editable = ['status']
+
+
+@admin.register(models.Color)
+class ColorAdmin(Default):
+    list_display = ['color']
+
+
+@admin.register(models.Status)
+class StatusAdmin(Default):
+    list_display = ['description']
