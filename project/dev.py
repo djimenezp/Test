0a309fafd4b58a922fcd0c0b7eea4e81b8e69fd3
David <d900929.jp@gmail.com>
2023-01-19 21:45:15 +0100

    Fixing dev environments

--- project/dev.py
+++ project/dev.py
@@ -1,9 +1,9 @@
 from .settings import *
 
 assert BASE_DIR
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", 'django-insecure-upnfygsoga2)v6ngphwzelinh58w&id$w&bik(k=)=yyb(@$mc')
 
-DEBUG = int(os.environ.get("DEBUG", default=0))
+DEBUG = int(os.environ.get("DEBUG", default=1))
 
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
